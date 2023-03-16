f18103a78071f426ff564cdf9859c8228e73a7ef
Marviel <lukebechtel4@gmail.com>
2023-03-15 23:04:40 -0400

    fix again

--- components/messages/GPT4Message.tsx
+++ components/messages/GPT4Message.tsx
@@ -1,9 +1,9 @@
 import Message from './Message'
 
 export default function GPT4Message({ children }: React.PropsWithChildren<any>) {
-    return (
-        <Message name="GPT-4" avatarUrl="/static/images/openai/gpt4-logo.svg">
-            {children}
-        </Message>
-    )
+  return (
+    <Message name="GPT-4" avatarUrl="/static/images/openai/gpt4-logo.svg">
+      {children}
+    </Message>
+  )
 }
