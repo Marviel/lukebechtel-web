a5a4fbed5b1375a057eebabb2c50865fd62ff0e0
Marviel <lukebechtel4@gmail.com>
2023-03-15 23:04:21 -0400

    lint fix

--- components/messages/GPT4Message.tsx
+++ components/messages/GPT4Message.tsx
@@ -1,9 +1,9 @@
 import Message from './Message'
 
-export default function GPT4Message({ children }: React.PropsWithChildren<{}>) {
-  return (
-    <Message name="GPT-4" avatarUrl="/static/images/openai/gpt4-logo.svg">
-      {children}
-    </Message>
-  )
+export default function GPT4Message({ children }: React.PropsWithChildren<any>) {
+    return (
+        <Message name="GPT-4" avatarUrl="/static/images/openai/gpt4-logo.svg">
+            {children}
+        </Message>
+    )
 }
