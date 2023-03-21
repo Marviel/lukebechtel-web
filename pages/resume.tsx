942577bd0ee50e3c07db6d7db9d8e599c22701fd
Marviel <lukebechtel4@gmail.com>
2023-03-21 19:37:46 -0400

    Fix resume on mobile

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -8,10 +8,14 @@ import Link from 'next/link'
 import url from 'pdfjs-dist/build/pdf.worker'
 import { Document, Page, pdfjs } from 'react-pdf'
 
+import { useMediaQuery } from '@mui/material'
+
 pdfjs.GlobalWorkerOptions.workerSrc = url
 
 export default function Resume() {
   const [numPages, setNumPages] = useState(null)
+  const isSmallDevice = useMediaQuery('(max-width: 600px)')
+  const isVerySmallDevice = useMediaQuery('(max-width: 400px)')
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages)
@@ -22,6 +26,14 @@ export default function Resume() {
       <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
         Resume
       </h1>
+      <h3>
+        <Link
+          href="/static/pdfs/luke_bechtel_current_resume.pdf"
+          style={{ textDecoration: 'underline' }}
+        >
+          Download
+        </Link>
+      </h3>
       <h3>
         <i>
           You can also see my{' '}
@@ -35,14 +47,16 @@ export default function Resume() {
           for more info :{`)`}
         </i>
       </h3>
-      <Document
-        file="/static/pdfs/luke_bechtel_current_resume.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        {_.range(1, numPages + 1).map((pageNumber) => (
-          <Page key={`page_${pageNumber}`} pageNumber={pageNumber} />
-        ))}
-      </Document>
+      <div style={{ zoom: isSmallDevice ? (isVerySmallDevice ? 0.5 : 0.7) : 1 }}>
+        <Document
+          file="/static/pdfs/luke_bechtel_current_resume.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          {_.range(1, numPages + 1).map((pageNumber) => (
+            <Page key={`page_${pageNumber}`} pageNumber={pageNumber} />
+          ))}
+        </Document>
+      </div>
     </div>
   )
 }
